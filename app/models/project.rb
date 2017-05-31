868a0b5782a7dc87d02a7b51cacdc13d0efa6d78
Guillermo <guillermijas@gmail.com>
2017-05-31 23:41:09 +0200

    validation in project and requirements

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,35 +1,35 @@
 class Project < ApplicationRecord
-  has_attached_file :picture, styles: { medium: "300x300", thumb: "100x100" }
-  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
-  attr_accessor :delete_picture
-  before_validation { picture.clear if delete_picture == '1' }
-
-
-
-  validates :name, presence: true, allow_blank: false
-  validates :status, inclusion: { in: %w(planning in_process on_hold verifying archived),
-                                message: "'%{value}' no es un estado válido" }
-
   has_many :user_projects, dependent: :destroy
   has_many :users, -> { distinct }, through: :user_projects
 
   has_many :requirements, dependent: :destroy
   has_one :glossary, dependent: :destroy
 
+  has_attached_file :picture, styles: { medium: "300x300", thumb: "100x100" }
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+
+  attr_accessor :delete_picture
+  before_validation { picture.clear if delete_picture == '1' }
+
+
+  validates :name, presence: true, allow_blank: false
+  validates :status, inclusion: { in: %w[planning in_process on_hold verifying archived],
+                                  message: "'%{value}' no es un estado válido" }
+
   def status_string
     case status
-      when 'planning'
-        'Planificando'
-      when 'in_process'
-        'En proceso'
-      when 'on_hold'
-        'En espera'
-      when 'verifying'
-        'Verificando'
-      when 'archived'
-        'Archivado'
-      else
-        'Error en el estado'
+    when 'planning'
+      'Planificando'
+    when 'in_process'
+      'En proceso'
+    when 'on_hold'
+      'En espera'
+    when 'verifying'
+      'Verificando'
+    when 'archived'
+      'Archivado'
+    else
+      'Error en el estado'
     end
   end
 
--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -2,4 +2,12 @@ class Requirement < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
   belongs_to :project
+
+  validates :project_id, presence: true
+  validates :user_id, presence: true
+  validates :level, inclusion: { in: %w[high medium low],
+                                 message: "'%{value}' no es un nivel válido" }
+  validates :status, inclusion: { in: %w[pending in_progress verifying rejected],
+                                  message: "'%{value}' no es un nivel válido" }
+
 end
