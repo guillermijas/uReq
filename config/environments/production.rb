68eea5aadb2aea2fea46696f6f7abd86419c61c2
Guillermo <guillermijas@gmail.com>
2017-07-11 15:49:45 +0200

    paperclip s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,7 @@ Rails.application.configure do
         access_key_id: ENV['AWS_ACCESS_KEY_ID'],
         secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     },
-    url: ':s3_domain_url'
+    url: ':s3_domain_url',
+    path: 'public/system/:class/:attachment/:id_partition/:style/:filename'
   }
 end
