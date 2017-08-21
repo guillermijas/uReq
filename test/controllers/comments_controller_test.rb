77a7fd153d7fae82c3ea1b4cdeadc1c1a3858b48
Guillermo Mora <guillermijas@gmail.com>
2017-08-21 19:29:29 +0200

    Tests coments

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -6,38 +6,21 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
     @requirement = requirements(:one)
     @project = projects(:ureq)
     @user = users(:guille)
-    @fer = users(:fer)
     @admin = users(:admin)
     sign_in(@user)
   end
 
   test 'should create comment' do
     assert_difference('Comment.count') do
-      post project_requirement_comment_path(@project, @requirement), params: { comment: { body: @comment.body } }
+      post project_requirement_comments_path(@project.id, @requirement.id), xhr: true, params: { comment: { body: @comment.body } }
     end
     assert_response :success
   end
-  #
-  #   test "should show comment" do
-  #     get comment_url(@comment)
-  #     assert_response :success
-  #   end
-  #
-  #   test "should get edit" do
-  #     get edit_comment_url(@comment)
-  #     assert_response :success
-  #   end
-  #
-  #   test "should update comment" do
-  #     patch comment_url(@comment), params: { comment: { body: @comment.body } }
-  #     assert_redirected_to comment_url(@comment)
-  #   end
-  #
-  #   test "should destroy comment" do
-  #     assert_difference('Comment.count', -1) do
-  #       delete comment_url(@comment)
-  #     end
-  #
-  #     assert_redirected_to comments_url
-  #   end
+
+  test 'should destroy comment' do
+    assert_difference('Comment.count', -1) do
+      delete project_requirement_comment_path(@project.id, @requirement.id, @comment.id), xhr: true
+    end
+    assert_response :success
+  end
 end
