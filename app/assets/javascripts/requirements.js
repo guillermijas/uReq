5b98072e7519a8cd9e6295420e37eea1cc0a28cd
Guillermo Mora <guillermijas@gmail.com>
2017-08-26 01:41:55 +0200

    Finished requirements. remove logs from reqs

--- app/assets/javascripts/requirements.js
+++ app/assets/javascripts/requirements.js
@@ -1,15 +1,12 @@
 CKEDITOR.config.customConfig = 'ck_config.js';
 
 $(document).ready(function(){
-    /*https://stackoverflow.com/questions/8279859/get-first-letter-of-each-word-in-a-string-in-javascript*/
-    function updateSuffix(){
-      $("#requirement_suffix").val('R'+$("#requirement_category").val().match(/\b\w/g).join('').toUpperCase());
-    }
+  /*https://stackoverflow.com/questions/8279859/get-first-letter-of-each-word-in-a-string-in-javascript*/
+  function updateSuffix(){
+    $("#requirement_suffix").val('R'+$("#requirement_category").val().match(/\b\w/g).join('').toUpperCase());
+  }
 
-    $(document).on("input", "#requirement_category", updateSuffix);
-
-    $('[data-toggle="popover"]').popover();
-
+  $(document).on("input", "#requirement_category", updateSuffix);
 });
 
 
